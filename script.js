49478fda2a25f4c7289a5637bb8806c0af2c187a
Talon Bragg <w1tchhunt@protonmail.com>
2019-05-23 14:56:37 -0700

    Added and deleted comments

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var canvas = document.getElementById('game');
 var ctx = canvas.getContext('2d');
 
+// Key Event Handling
 var rightPressed = false;
 var leftPressed = false;
 
@@ -29,6 +30,7 @@ var xSpawnPoints = [-50, -10, 30, 70, 110, 150, 190, 230, 270, 310, 350, 390, 43
 
 var restartbutton = new Image();
 
+// Slime Character
 var slime = {
     x: 335,
     y: 475,
@@ -37,6 +39,7 @@ var slime = {
     alive: true
 }
 
+// Basic Game Variables
 var textVars = {
     score: 0,
     anvilSpawnRate: 1000,
@@ -44,6 +47,7 @@ var textVars = {
     gameStarted: false
 }
 
+// Anvil Object
 class Anvil {
     constructor(x, height, width) {
         this.x = x;
@@ -61,35 +65,43 @@ class Anvil {
     }
 }
 
+// Draws Slime
 function drawSlime() {
     ctx.fillStyle = 'rgb(0, 255, 0)';
     ctx.fillRect(slime.x, slime.y, slime.width, slime.height);
 }
 
+// All of the anvils that are onscreen
 var anvils = [];
 
 // xSpawnPoints[Math.floor(Math.random()*xSpawnPoints.length)] + 50
 
+// Adds a new anvil to the anvils[] array every 35ms
 setInterval(function() {
     if (anvils.length <= 2000) {
         anvils.push(new Anvil(xSpawnPoints[Math.floor(Math.random() * xSpawnPoints.length)] + 50, 25, 25));
     }
 }, 35);
 
+// Loads the restart button for when your characer dies
 restartbutton.onload = function () {
   textVars.restartButtonLoaded = true;
 };
 
+// Game Started so run this function
 function gameStarted() {
+  // Main game loop
   setInterval(function() {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+      //Score text
       ctx.font = "20px Arial";
       ctx.fillText("Score" + " " + textVars.score, 600, 50);
-
+      
+      //Slime is alive so complete every function needed to run the game
       if (slime.alive == true) {
           drawSlime();
-
+          
+          //Keypressed
           if (rightPressed && slime.x < canvas.width - slime.height) {
               slime.x += 5;
           }
@@ -97,30 +109,35 @@ function gameStarted() {
           if (leftPressed && slime.x > 0) {
               slime.x -= 5;
           }
+          //Draw anvils if the slime is alive
           if (slime.alive == true) {
               for (let i = 0; i < anvils.length; i++) {
-                  // console.log(anvils[i].x);
+                  //Draw single anvil
                   anvils[i].draw();
-                  // anvils[i].y += 1;
+                  //Setting the gravity of all of the anvils
                   anvils[i].gravitySpeed += anvils[i].gravity;
                   anvils[i].y += anvils[i].speedY + anvils[i].gravitySpeed;
+                  //If anvils has touched the bottom
                   if (anvils[i].y > 475) {
                       anvils[i].gravitySpeed = 0;
                       anvils[i].gravity = 0;
                       anvils[i].speedY = 0;
+                      //Remove anvil from array
                       anvils.splice(i, 1);
                       if (slime.alive == true) {
                           textVars.score += 1;
                       }
                   }
+                  //Collision detection of slime and anvil
                   if (anvils[i].x < slime.x + slime.width && anvils[i].x + anvils[i].width > slime.x && anvils[i].y < slime.y + slime.height && anvils[i].y + anvils[i].height > slime.y && anvils[i].y < 475) {
+                      //Slime set to dead so therefore game over
                       slime.alive = false;
                   }
               }
           }
-
       }
-
+      
+      //Slime is dead and game over
       if (slime.alive == false) {
           ctx.beginPath();
           ctx.font = "20px Arial";
@@ -174,50 +191,3 @@ function gameStarted() {
   }, 10);
 }
 gameStarted();
-// if (textVars.gameStarted == false) {
-//   //Start Screen
-//   ctx.beginPath();
-//   ctx.font = "30px Arial";
-//   ctx.fillStyle = 'rgb(255,255,255)'
-//   ctx.fillText('Heavy Anvil', 275, 225);
-//   ctx.closePath();
-
-//   function getMousePos(canvas, event) {
-//     var rect = canvas.getBoundingClientRect();
-//     return {
-//         x: event.clientX - rect.left,
-//         y: event.clientY - rect.top
-//     };
-//   }
-//   //Function to check whether a point is inside a rectangle
-//   function isInside(pos, rect) {
-//       return pos.x > rect.x && pos.x < rect.x + rect.width && pos.y < rect.y + rect.height && pos.y > rect.y
-//   }
-//   //The rectangle should have x,y,width,height properties
-//   var rect = {
-//       x: 300,
-//       y: 300,
-//       width: 100,
-//       height: 50
-//   };
-//   // Binding the click event on the canvas
-//   canvas.addEventListener('click', function(evt) {
-//       var mousePos = getMousePos(canvas, evt);
-
-//       if (isInside(mousePos, rect)) {
-
-//           console.log('click inside');
-//       } else {
-//           //nothing needs to happen here
-//           console.log('click outside');
-//       }
-//   }, false);
-
-//   var startButton = new Image();
-
-//   startButton.onload = function() {
-//     ctx.drawImage(startButton, rect.x, rect.y);
-//   };
-
-//   startButton.src = './startbutton.png'
-// }
